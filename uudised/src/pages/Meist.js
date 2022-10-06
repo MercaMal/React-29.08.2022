2a7458844f0f65a9e43055652c4fda8db923db04
MercaMal <111385064+MercaMal@users.noreply.github.com>
2022-10-06 16:51:08 +0300

    nimede kuvamine exceliga-kodutöö nr 17

--- uudised/src/pages/Meist.js
+++ uudised/src/pages/Meist.js
@@ -1,11 +1,65 @@
-import {useState} from "react"
+import {useState} from "react";
 function Meist () {
     const [kontakt, n2itaKontakt] =  useState("")
+    const tootajad = [
+        {
+            "Nimi": "Merike Mõksi",
+            "Ala": "Uudisklippide taustamuusika",
+            "Telefon": "55650964"
+        },
+        {
+            "Nimi": "Pille Minev",
+            "Ala": "Püstolreporter",
+            "Telefon": "5178975"
+        },
+        {
+            "Nimi": "Hanna Martinson",
+            "Ala": "Kujundus",
+            "Telefon": "5049532"
+        },
+        {
+            "Nimi": "Kerli Dello",
+            "Ala": "Välisturgude spetsialist",
+            "Telefon": "5550500"
+        },
+        {
+            "Nimi": "Pille Minev",
+            "Ala": "Püstolreporter",
+            "Telefon": "5178975"
+        },
+        {
+            "Nimi": "Hanna Martinson",
+            "Ala": "Kujundus",
+            "Telefon": "5049532"
+        },
+        {
+            "Nimi": "Kerli Dello",
+            "Ala": "Välisturgude spetsialist",
+            "Telefon": "5550500"
+        },
+
+        
+        
+    ]
     return ( <div>
+
         <div>See on meist leht, nähtav localhost:3000/meist aadressil</div> 
         <div className="tekst">Meie töötajad</div>
         <br />
-        <div className="tekst">Merike Mõksi</div>
+
+        {tootajad.map(tootaja=>
+          <div>
+           <div>{tootaja.Nimi}</div>
+           <div>{tootaja.Ala}</div>
+        
+           <button onClick={() =>n2itaKontakt(tootaja.Telefon)}>Võta ühendust</button>
+           </div> )} 
+           <br/>
+          { kontakt !==""&& <div>Tema kontakt: {kontakt}</div>} 
+       
+
+
+        {/* <div className="tekst">Merike Mõksi</div>
         <div>Uudisteklippide taustamuusika</div>
         <button onClick={()=> n2itaKontakt (5178675)}>Võta ühendust</button>
         <br /> <br />
@@ -18,12 +72,10 @@ function Meist () {
         <button onClick={() =>n2itaKontakt(55650964)}>Võta ühendust</button>
         <br /> <br />
         <div>Kerli Dello</div>
-        <div>Välisturgude spetsialist</div>
-        <button onClick={() =>n2itaKontakt(5049532)}>Võta ühendust</button>
-        <br /> <br />
-         { kontakt !==""&& <div>Tema kontakt: {kontakt}</div>}
-
-    </div> );
+        <div>Välisturgude spetsialist</div> */}
+       
+       </div>
+       );
 }
 
 export default Meist ;
