37792ed283d24ff5024b1343ec0979d55350fe1d
MercaMal <111385064+MercaMal@users.noreply.github.com>
2022-10-19 20:38:24 +0300

    kujundus ja fetch, useEffect 19.10.tund

--- webshop/src/pages/Cart.jsx
+++ webshop/src/pages/Cart.jsx
@@ -1,9 +1,23 @@
+import { useEffect } from "react";
+import { useRef } from "react";
 import { useState } from "react";
-import Button from "react-bootstrap/Button";
 import styles from "../css/Cart.module.css";
+
 
 function  Cart () {
 const [cart, setCart] = useState (JSON.parse(localStorage.getItem("cart")) || [] );
+const [parcelMachines, setParcelMachines] = useState ([]); // []-tähistavad hetke, millal ta saab asjad kätte, 
+const pmRef = useRef();                                                               //tühi massiiv hetkel. 0-0,5 sek on asi tühi
+                                                                              //panakse loaderid käima, kui asi lõppeb, siis loader kaob.
+                                                                              // seni kuni laeb, siis on tühi, kui kandilist tühja ei pane, siis tuleb error 
+
+ useEffect(()=> {
+ fetch("https://www.omniva.ee/locations.json") //päringu tegemine lehele
+ .then(res => res.json()) // pane ta json kujule
+ .then(json => setParcelMachines(json)) //saan json kujule, siis json setParcelMachine ja see omakorda lähen parcelMachine sisse
+}, [])
+
+
  const remove = (index)=> {
     cart.splice(index,1);
     setCart(cart.slice ());
@@ -22,7 +36,10 @@ const increaseQuantity = (productIndex) => {
     cart[productIndex].quantity = cart[productIndex].quantity + 1;
     setCart(cart.slice());
 }
-
+ const sendOrder = () => {
+    console.log(pmRef.current.value);
+    console.log(cart);
+ }
 return ( 
     <div>
      {cart.map((element, index)=>
@@ -30,17 +47,36 @@ return (
         <img src={element.product.image} alt=""/>
         <div className={styles.name}>{element.product.name}</div>
         <div className={styles.price}>{element.product.price} £ </div>
-        <button onClick={()=>decreaseQuantity(index)}>-</button>
-        <div className={styles.quantity}>{element.quantity} tk</div>
-        <button onClick={()=>increaseQuantity(index)}>+</button>
+        <div className={styles.quantity}>
+          <img className={styles.button} onClick={()=>decreaseQuantity(index)} src={require("../images/minus.png")} alt=""/>
+          <div>{element.quantity} tk</div>
+          <img className={styles.button} onClick={()=>increaseQuantity(index)}src={require("../images/plus.png")} alt=""/>
+        </div>
+        
         <div className={styles.sum}>{ (element.product.price*element.quantity).toFixed(2)} £ </div>
-
-        <Button onClick={()=>remove(index)} variant= "danger">Kustuta</Button>
+        <img className={styles.button} onClick={()=>remove(index)}src={require("../images/x.png")} alt=""/>
         </div>  
         )}
-    <div> Kokku: {calculateCartSum()} £</div>
-    
+   <div className={styles.cart__bottom}>
+   <div> Kokku: {calculateCartSum()} £</div>
+
+  <select ref={pmRef}>{parcelMachines
+  .filter(element => element.A0_NAME === "EE")
+ .map(element=>
+ <option key={element.NAME}>{element.NAME}</option>)}
+ </select>
+
+ <button onClick={sendOrder}>Vormista tellimus</button>
+ 
+
+
+   </div>
     </div> );
 }
 
 export default  Cart ;
+/* oskad kommunikeerida, rääkida arendajatega
+ /* udemist trikke juurde vaadata
+ /* kui cssi impordin, siis kehtib üle terve projekti
+ /* .module on arendajatele justkui märksõna. iga faili sjaoks on üks css
+ /* moodul. ilma mooduliteta on ta igas failis*/
