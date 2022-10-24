9279a1aa57a3d1dfaca1bbf49545b78edd06c174
MercaMal <111385064+MercaMal@users.noreply.github.com>
2022-10-24 19:05:24 +0300

    API päring+kujundus

--- uudised/src/pages/Avaleht.jsx
+++ uudised/src/pages/Avaleht.jsx
@@ -1,10 +1,35 @@
+import { useState, useEffect } from "react";
+import styles from "../css/Avaleht.module.css"
+
 function Avaleht () {
+  const [postitused, uuendaPostitused] = useState([]);
+  useEffect(() => {
+    
+    fetch("https://jsonplaceholder.typicode.com/posts")
+    .then(res => res.json())
+    .then(data => uuendaPostitused(data))
+
+}, []);
+
+
     return ( <div>
     
      <div>See on avaleht, nähtav localhost:3000 aadressil</div> 
-     <div className="tekst">Tere tulemast avalehele</div>
+     <div>Tere tulemast avalehele</div>
+
      <img className="avalehe-pilt" src="https://g.nh.ee/images/pix/1600x914/LSr3GFcB35A/aktuaalne-kaamera-err-err-uudistetoimetus-etv-86926649.jpg?up=1" alt="uudistetoimetus" />
-    
+     
+     {postitused.map(element =>
+
+      <div className={styles.posts}> 
+
+       <div className={styles.userId}>{element.userId}</div>
+       <div className={styles.id}>{element.id}</div>
+       <div className={styles.title}>{element.title}</div>
+       <div className={styles.body}>{element.body}</div>
+      </div>
+      )}
+
     </div>)
 
 }
