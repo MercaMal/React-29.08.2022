a7ba3a1e3a6250490cd64560841695d701512206
MercaMal <111385064+MercaMal@users.noreply.github.com>
2022-10-24 19:05:46 +0300

    Payment

--- veebipood/src/pages/Ostukorv.js
+++ veebipood/src/pages/Ostukorv.js
@@ -24,8 +24,30 @@ const arvutaOstukorviKogusumma = () => {
  }
  
 
+const pay = () => {
 
+    const data = {
+        "api_username": "92ddcfab96e34a5f",
+        "account_name": "EUR3D1",
+        "amount":arvutaOstukorviKogusumma(),
+        "order_reference": Math.random() * 99999,
+        "token_agreement": "unscheduled",
+        "nonce": "a9b7f7e792"+ Math.random() * 99999+ new Date (),
+        "timestamp": new Date (),
+        "email": "user@example.com",
+        "customer_url": "https://merca-react-project"
+        }
+       fetch("https://igw-demo.every-pay.com/api/v4/payments/oneoff",{
+        "method": "POST",
+         "body": JSON.stringify(data),
+         "headers":{
+            "Content-Type": "application/json",
+            "Authorization": "Basic OTJkZGNmYWI5NmUzNGE1Zjo4Y2QxOWU5OWU5YzJjMjA4ZWU1NjNhYmY3ZDBlNGRhZA=="
+         }
+        }) .then(res=> res.json())
+        .then(json =>window.location.href=json.payment_link);
 
+}
     return ( 
     <div>
         {ostukorv.length > 0 && <div>Ostukorvis on {ostukorv.length} eset</div>}
@@ -42,6 +64,7 @@ const arvutaOstukorviKogusumma = () => {
         <button onClick={()=>kustuta(index)}>x</button>
          </div>)}
          <div>{arvutaOstukorviKogusumma()}£</div>
+            <button onClick = {pay}>Vormista tellimus</button>
 
     </div> );
 }
